952119876fd99fe256f4e854e9d843ed359f4187
tom hannaford <thom.hannaford@gmail.com>
2019-12-24 21:57:21 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,7 +2,6 @@
 # For output purposes, use "puts" instead of "print" or "p"
 def meal_choice(veg1, veg2, protein="meat")
   introduction = "What a nutritious meal!"
-  puts introduction
   visitor_order = "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
