c449a1a4f55ae8d44b249eb847b82e69ff260fb9
tom hannaford <thom.hannaford@gmail.com>
2019-12-24 21:52:35 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,9 +6,9 @@ def meal_choice(veg1, veg2, protein="meat")
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
   visitor_order = "#{veg1}, #{veg2}, #{protein}"
-  visitor_meals.push(visitor_order)
+  visitor_meals >> visitor_order
 end
 
 meal_choice("broccoli", "macaroni")
-  
+
   
