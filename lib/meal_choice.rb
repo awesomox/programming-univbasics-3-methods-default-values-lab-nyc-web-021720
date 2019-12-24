6f099b3582a4578e77d8a0d11ffab785d2f198b3
tom hannaford <thom.hannaford@gmail.com>
2019-12-24 22:34:37 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,5 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
-introduction = "What a nutritious meal!"
 
 def meal_choice(veg1, veg2, protein="meat")
   puts introduction
